3a5d63856792cfd80f6e5f1575388d4e678a5cac
Abhishek Tamrakar <abhishektamrakar013@gmail.com>
2025-04-09 04:13:57 +0530

    enhancement : added swagger for code documentation

--- pms_project/settings.py
+++ pms_project/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pms_app',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
